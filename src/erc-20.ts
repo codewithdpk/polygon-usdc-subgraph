f3d60992a71d0384516da1b7e97eb9dcb9df982f
Deepak Suthar <codewithdeepak@yahoo.com>
2024-03-04 15:08:01 +0530

    feat: initial commit

--- src/erc-20.ts
+++ src/erc-20.ts
@@ -1,32 +1,62 @@
+//import event class from generated files
+import {Transfer} from "../generated/ERC20/ERC20"
+//import entities
+import {TokenBalance} from "../generated/schema"
+//import the functions defined in utils.ts
 import {
-  AdminChanged as AdminChangedEvent,
-  Upgraded as UpgradedEvent
-} from "../generated/ERC20/ERC20"
-import { AdminChanged, Upgraded } from "../generated/schema"
+  fetchTokenDetails,
+  fetchAccount,
+  fetchBalance
+} from "./utils"
+//import datatype
+import { BigDecimal} from "@graphprotocol/graph-ts";
 
-export function handleAdminChanged(event: AdminChangedEvent): void {
-  let entity = new AdminChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousAdmin = event.params.previousAdmin
-  entity.newAdmin = event.params.newAdmin
+export function handleTransfer(event: Transfer): void {
+    let token = fetchTokenDetails(event);
+    if (!token) { //if token == null
+        return
+      }
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+    //get account addresses from event
+    let fromAddress = event.params.from.toHex();
+    let toAddress = event.params.to.toHex();
 
-  entity.save()
-}
+    //fetch account details
+    let fromAccount = fetchAccount(fromAddress);
+    let toAccount = fetchAccount(toAddress);
 
-export function handleUpgraded(event: UpgradedEvent): void {
-  let entity = new Upgraded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.implementation = event.params.implementation
+    if (!fromAccount || !toAccount) {
+    return;
+    }
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+    //setting the token balance of the 'from' account
+    let fromTokenBalance = TokenBalance.load(token.id + "-" + fromAccount.id);
+    if (!fromTokenBalance) { //if balance is not already saved
+					//create a new TokenBalance instance
+					// while creating the new token balance,
+					// the combination of the token address 
+					// and the account address is  
+					// passed as the identifier value
+          fromTokenBalance = new TokenBalance(token.id + "-" + fromAccount.id);
+          fromTokenBalance.token = token.id;
+          fromTokenBalance.account = fromAccount.id;
+    }
 
-  entity.save()
+    fromTokenBalance.amount = fetchBalance(event.address,event.params.from)
+		//filtering out zero-balance tokens - optional
+    if(fromTokenBalance.amount != BigDecimal.fromString("0")){
+      fromTokenBalance.save();
+    }
+    
+    //setting the token balance of the 'to' account
+    let toTokenBalance = TokenBalance.load(token.id + "-" + toAccount.id);
+    if (!toTokenBalance) {
+        toTokenBalance = new TokenBalance(token.id + "-" + toAccount.id);
+        toTokenBalance.token = token.id;
+        toTokenBalance.account = toAccount.id;
+      }
+    toTokenBalance.amount = fetchBalance(event.address,event.params.to)
+    if(toTokenBalance.amount != BigDecimal.fromString("0")){
+      toTokenBalance.save();
+    }
 }
